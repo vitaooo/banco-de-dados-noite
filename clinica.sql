787c159de1b23baf7d9293625ef218897b53d1e1
Victor <victorhvercosag@gmail.com>
2025-09-15 21:57:27 -0300

    clinica

--- clinica.sql
+++ clinica.sql
@@ -12,12 +12,10 @@ med_telefone varchar(11), med_especialidade int not null,
 primary key(id_crm),
 foreign key (med_especialidade) references especialidades(idespecialidade));
 
-drop table medicos;
-
 create table if not exists especialidades(idespecialidade int not null,
 esp_nome varchar(20), primary key(idespecialidade));
 
-create table remedios(idremedios int not null,
+create table if not exists remedios(idremedios int not null,
 rem_nome varchar(45),
 primary key(idremedios));
 
@@ -27,7 +25,7 @@ alter table remedios drop rem_nome;
 
 alter table remedios add rem_nome varchar(45) not null;
 
-create table consultas(id_consulta int not null,
+create table if not exists consultas(id_consulta int not null,
 con_data datetime,
 medicos_idcrm int not null,
 pacientes_idpaciente int not null,
@@ -35,7 +33,7 @@ primary key (id_consulta),
 foreign key(medicos_idcrm) references medicos(id_crm),
 foreign key(pacientes_idpaciente) references pacientes(idpaciente));
 
-create table consultas_has_remedios(
+create table if not exists consultas_has_remedios(
 id_consulta int not null,
 idremedios int not null,
 cor_posologia varchar(45),
@@ -43,4 +41,37 @@ primary key (id_consulta, idremedios),
 foreign key(id_consulta)
 	references consultas(id_consulta),
 foreign key(idremedios)
-	references remedios(idremedio));
+	references remedios(idremedios));
+    
+insert into especialidades values (2, "Gastro");
+insert into especialidades values (3, "Cardiologista");
+
+insert into medicos values (1, "dotor", "av manoel goulart 221", "181818",1);
+insert into medicos values (2, "dotour", "av manoel golart 222", "171717",2);
+
+insert into pacientes values (4,"maria","endec 1","111111111"), (5,"mario","endec 2","22222222"), (6,"marii","endec 3","33333333"), (7,"mariu","endec 4","444444444");
+
+insert into consultas values (1, "2025-09-15 19:00:00",2,2);
+
+insert into consultas values (2, current_time(),2,2);
+
+insert into remedios values(1, "ozempic"), (2, "dipirona"), (3,"Nimesulida"), (4,"Buscopan");
+
+insert into consultas_has_remedios (id_consulta, idremedios) values(1,2),(2,3),(3,3);
+
+insert into consultas_has_remedios values (1,2,""),(2,3,"");
+
+select * from especialidades;
+select * from medicos;
+select * from pacientes;
+select * from consultas;
+select * from remedios;
+select * from consultas_has_remedios;
+
+update consultas_has_remedios set cor_posologia = "3 8/8" where id_consulta = 1 and idremedios = 2;
+
+update consultas_has_remedios set cor_posologia = "1 com pela manhã" where id_consulta = 2 and idremedios = 3;
+
+update consultas_has_remedios set cor_posologia = "beber" where id_consulta <> 1;
+
+update consultas_has_remedios set cor_posologia = "DISSOLVER NA AGUA" where id_consulta = 1 or idremedios = 3;
