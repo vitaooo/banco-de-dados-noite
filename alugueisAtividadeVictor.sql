894449e26faff0732399e5f14a7f03d102697d6a
Victor <victorhvercosag@gmail.com>
2025-10-13 22:07:04 -0300

    feito até o 9

--- alugueisAtividadeVictor.sql
+++ alugueisAtividadeVictor.sql
@@ -1,6 +1,8 @@
 -- ex 1
-use alugueis;
 select nome, contato from clientes order by nome;
+
+-- ex 2
+select * from clientes order by cliente_id desc;
 
 -- ex 3 (as quantidade == nome da tabela que conta as linhas
 select count(nome) from clientes;
@@ -8,6 +10,27 @@ select count(nome) from clientes;
 -- ex 4
 select count(nome) as totalsilva from clientes where nome LIKE '%SILVA%';
 
+-- ex 5
+select distinct estado from enderecos order by estado;
+
 -- ex 6
 select distinct estado, cidade from enderecos 
 order by estado, cidade;
+
+-- ex 7
+SELECT distinct CONCAT(cidade, estado) as cidade_estado FROM enderecos;
+
+-- ex 9
+select * from enderecos where numero >= 100 and numero <= 200;
+-- or
+select * from enderecos where 
+numero between 100 and 200;
+
+-- ex 11
+select p.proprietario_id, p.nome, p.contato, 
+h.tipo, 
+e.rua, e.numero, e.bairro, e.cidade, e.estado 
+from proprietarios p
+inner join hospedagens h on p.proprietario_id = h.proprietario_id
+inner join enderecos e on e.endereco_id = h.endereco_id
+where e.estado = 'SP';
